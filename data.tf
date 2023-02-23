874b59d38cb1f699579bac2931ad4328a60fe137
George Chan <gchan@visuallease.com>
2023-02-22 22:24:25 -0500

    adding secret mgr

--- data.tf
+++
@@ -1,6 +0,0 @@
-data "aws_secretsmanager_secret" "jenkins_secrets" {
-  name = "${var.ENVIRONMENT}/jenkins/automation"
-}
-data "aws_secretsmanager_secret_version" "jenkins_secrets_version" {
-  secret_id = data.aws_secretsmanager_secret.jenkins_secrets.id
-}
